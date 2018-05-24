7d5c37fde55364185cc748e9dd67ac4e8f898b56
Justin Kimbrell <justin@objectivehtml.com>
2018-05-23 21:13:14 -0600

    Bug Fix
    - Added the ability to parse plain text as a component

--- src/Helpers/Instantiate/Instantiate.js
+++ src/Helpers/Instantiate/Instantiate.js
@@ -1,4 +1,5 @@
 import { isObject } from 'lodash-es';
+import { isString } from 'lodash-es';
 
 export default function instantiate(Vue, Component, options) {
     if(Component instanceof Vue) {
@@ -7,6 +8,19 @@ export default function instantiate(Vue, Component, options) {
 
     if(isObject(Component)) {
         Component = Vue.extend(Component);
+    }
+    else if(isString(Component)) {
+        const text = Component;
+
+        Component = Vue.extend({
+
+            functional: true,
+
+            render(h, context) {
+                return this._v(text);
+            }
+
+        });
     }
 
     return new Component(options);
