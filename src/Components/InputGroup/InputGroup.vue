f94bff9a49dc173762574e0aa867d572fb27c55d
Justin Kimbrell <jkimbrell@actengage.com>
2018-11-12 14:09:27 -0700

    0.2.57

--- src/Components/InputGroup/InputGroup.vue
+++ src/Components/InputGroup/InputGroup.vue
@@ -67,11 +67,15 @@ export default {
 </script>
 
 <style lang="scss">
-.input-group > div:not(:first-child) .form-control {
+.input-group > div:not(:first-child) .form-control,
+.input-group > div:not(:first-child) .custom-control,
+.input-group > div:not(:first-child) .custom-select {
     border-top-left-radius: 0;
     border-bottom-left-radius: 0;
 }
-.input-group > div:not(:last-child) .form-control {
+.input-group > div:not(:last-child) .form-control,
+.input-group > div:not(:last-child) .custom-control,
+.input-group > div:not(:last-child) .custom-select, {
     border-top-right-radius: 0;
     border-bottom-right-radius: 0;
 }
