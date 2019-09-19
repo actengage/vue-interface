3022f3029340103ee911864f63dd1eb03ee12455
Justin Kimbrell <justinkimbrell@Justins-MacBook-Pro.local>
2019-09-18 21:22:40 -0600

    pre-release commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
-console.log(123);
-
 export * from './Components';
-
-/*
 export * from './Directives';
 export * from './Filters';
 export * from './Helpers';
 export * from './Http';
 export * from './Mixins';
 export * from './Plugins';
-*/
