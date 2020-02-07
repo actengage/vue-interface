0901e8c73c10aab91dc0b362b3ad60cccca1cec3
Justin Kimbrell <justinkimbrell@Justins-MacBook-Pro-2.local>
2020-02-06 18:39:33 -0700

    pre-release commit

--- src/Directives/index.js
+++ src/Directives/index.js
@@ -1,13 +1,11 @@
 import Autogrow from './Autogrow';
 import Collapse from './Collapse';
-import InputList from './InputList';
 import Lazy from './Lazy';
 import Slug from './Slug';
 
 export {
     Autogrow,
     Collapse,
-    InputList,
     Lazy,
     Slug
 };
